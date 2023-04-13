82a0835cd69f47ec8ba2d747681e554e03e3fd59
Ganesh Jadhav <ganesh20215@gmail.com>
2023-04-13 09:59:31 +0530

    ksdjfhks

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(10*10); //100
- 
+
         System.out.println(50%10);  //5
         System.out.println(10+10); //20
         System.out.println(10-10); //0
         System.out.println(10-10); //0
         System.out.println(10-10); //0
         System.out.println(10-10); //0
-
+        System.out.println("Hello");
 
     }
 }
